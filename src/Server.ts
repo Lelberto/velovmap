b85f711b8c020aea9ef060f3bf1c807c1604327c
Lelberto <jsurieux@gmail.com>
2019-04-01 10:54:09 +0200

    Added comments

--- src/Server.ts
+++ src/Server.ts
@@ -5,10 +5,20 @@ import MongoDBService from './services/MongoDBService';
 import DataRetrieveService from './services/DataRetrieveService';
 import HomepageController from './controllers/HomepageController';
 
+/**
+ * Classe gérant le serveur.
+ * 
+ * Le serveur est le composant principal de l'application.
+ */
 export default class Server {
 
     public static INSTANCE: Server;
 
+    /**
+     * Retourne l'instance du serveur (Singleton).
+     * 
+     * L'instance sera créée quand cette méthode sera appelée pour la première fois.
+     */
     public static getInstance(): Server {
         if (!Server.INSTANCE) {
             Server.INSTANCE = new Server();
@@ -20,12 +30,20 @@ export default class Server {
     private readonly container: ServiceContainer;
     private readonly controllers: Controller[];
 
+    /**
+     * Construit un nouveau serveur.
+     */
     private constructor() {
         this.app = this.createExpressApplication();
         this.container = this.createServiceContainer();
         this.controllers = this.createControllers();
     }
 
+    /**
+     * Démarre le serveur.
+     * 
+     * @param port Port d'écoute (80 par défaut)
+     */
     public start(port: number = 80): void {
         this.container.mongodb.connect().then(() => {
             console.log('Connected to MongoDB Database');
@@ -38,6 +56,11 @@ export default class Server {
         }).catch(console.error);
     }
 
+    /**
+     * Crée l'application Express.
+     * 
+     * @returns Application Express créée
+     */
     private createExpressApplication(): Express.Application {
         const app = Express();
 
@@ -55,6 +78,11 @@ export default class Server {
         return app;
     }
 
+    /**
+     * Crée le conteneur de services.
+     * 
+     * @returns Conteneur de services créé
+     */
     private createServiceContainer(): ServiceContainer {
         const container: ServiceContainer = {};
         
@@ -64,6 +92,11 @@ export default class Server {
         return container;
     }
 
+    /**
+     * Crée les contrôleurs.
+     * 
+     * @returns Contrôleurs créés
+     */
     private createControllers(): Controller[] {
         const controllers: Controller[] = [
             new HomepageController(this.container)
